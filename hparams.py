Refactor, use uint8 and bfloat16 to store intermediate states, fixed ucb calculation, hyperparameter updates

hparams.py
@@ -14,17 +14,21 @@ class GenericHparams:
     num_actions: int
     device: torch.device = torch.device('cpu')
     dtype: torch.dtype = torch.float32
+    game_state_dtype: torch.dtype = torch.int8
 
     max_episode_len: int
     num_simulations: int
 
     default_reward: float = 0.0
 
-    discount: float = 1.0
-    c1: float = 1.25
-    c2: float = 19652
+    ucb_value_score_discount: float = 1
+    value_discount: float = 1
+    #pb_c_init: float = 1.25
+    pb_c_init: float = 4
+    pb_c_base: float = 19652
     add_exploration_noise: bool = True
-    dirichlet_alpha: float = 0.3
+    #dirichlet_alpha: float = 0.3
+    dirichlet_alpha: float = 1
     exploration_fraction: float = 0.25
 
     player_ids: List[int] = [1, 2]
@@ -32,12 +36,12 @@ class GenericHparams:
     num_unroll_steps: int = 5
     td_steps: int
 
-    max_training_games: int = 1
+    max_training_games: int = 50
 
     server_port: int = 50051
     num_server_workers: int = 2
 
-    num_training_steps: int = 2
+    num_training_steps: int = 10
     min_lr = 1e-5
     init_lr = 1e-4
 
@@ -69,8 +73,8 @@ class TicTacToeHparams(GenericHparams):
     state_shape: List[int] = [3, 3]
     num_actions: int = 9
 
-    max_episode_len: int = 10
+    max_episode_len: int = 9
     num_simulations: int = 800
 
     num_unroll_steps: int = 5
-    td_steps: int = 42
+    td_steps: int = 9

mcts.py
@@ -104,11 +104,11 @@ class Tree:
 
         self.saved_children_index = torch.zeros([self.hparams.batch_size, max_size]).long().to(hparams.device)
         self.visit_count = torch.zeros([self.hparams.batch_size, max_size]).long().to(hparams.device)
-        self.value_sum = torch.zeros([self.hparams.batch_size, max_size]).float().to(hparams.device)
-        self.prior = torch.zeros([self.hparams.batch_size, max_size]).float().to(hparams.device)
-        self.reward = torch.zeros([self.hparams.batch_size, max_size]).float().to(hparams.device)
+        self.value_sum = torch.zeros([self.hparams.batch_size, max_size], dtype=torch.bfloat16).to(hparams.device)
+        self.prior = torch.zeros([self.hparams.batch_size, max_size], dtype=torch.bfloat16).to(hparams.device)
+        self.reward = torch.zeros([self.hparams.batch_size, max_size], dtype=torch.bfloat16).to(hparams.device)
         self.expanded = torch.zeros([self.hparams.batch_size, max_size]).bool().to(hparams.device)
-        self.player_id = torch.zeros([self.hparams.batch_size, max_size]).long().to(hparams.device)
+        self.player_id = torch.zeros([self.hparams.batch_size, max_size], dtype=torch.uint8).to(hparams.device)
         self.player_id[:, 0] = player_id.detach().clone().to(self.hparams.device)
         self.hidden_states = {}
 
@@ -139,28 +139,30 @@ class Tree:
         #              f'children_index:\n{children_index[:max_debug]}')
         return children_index
 
-    def expand(self, player_id: torch.Tensor, batch_index: torch.Tensor, node_index: torch.Tensor, policy_logits: torch.Tensor):
+    def expand(self, player_id: torch.Tensor, batch_index: torch.Tensor, parent_index: torch.Tensor, policy_logits: torch.Tensor):
         if len(batch_index) != len(self.saved_children_index):
             # max_debug = 10
             # self.logger.critical(f'expand: batch_index: {batch_index.shape}\n{batch_index[:max_debug]}\n'
             #                   f'saved_children_index: {self.saved_children_index.shape}\n{self.saved_children_index[:max_debug]}\n'
-            #                   f'node_index: {node_index.shape}\n{node_index[:max_debug]}')
+            #                   f'parent_index: {parent_index.shape}\n{parent_index[:max_debug]}')
             raise ValueError(f'invalid batch index')
 
-        node_index = node_index.unsqueeze(1)
+        parent_index = parent_index.unsqueeze(1)
 
         children_index = self.new_children_index(batch_index)
-        self.saved_children_index.scatter_(1, node_index, self.simulation_index)
+        self.saved_children_index.scatter_(1, parent_index, self.simulation_index)
 
         # max_debug = 10
-        # self.logger.info(f'expand: generation: {self.simulation_index}, node_index: {node_index[:max_debug]}, children_index:\n{children_index[:max_debug]}')
+        # self.logger.info(f'expand: generation: {self.simulation_index}, player_id: {player_id[:max_debug]}\n'
+        #                  f'parent_index:\n{parent_index[:max_debug]}\n'
+        #                  f'children_index:\n{children_index[:max_debug]}')
 
-        self.expanded.scatter_(1, node_index, True)
-        self.player_id.scatter_(1, node_index, player_id.unsqueeze(1))
+        self.expanded.scatter_(1, parent_index, True)
+        self.player_id.scatter_(1, parent_index, player_id.unsqueeze(1))
 
-        probs = torch.softmax(policy_logits, 1)
+        probs = torch.softmax(policy_logits, 1).type(self.prior.dtype)
         self.prior.scatter_(1, children_index, probs)
-        #self.logger.debug(f'expand: priors:\n{self.prior[batch_index].gather(1, children_index)[:max_debug]}')
+        #self.logger.info(f'expand: priors:\n{self.prior[batch_index].gather(1, children_index)[:max_debug]}')
 
         self.simulation_index += 1
 
@@ -170,45 +172,48 @@ class Tree:
 
         return torch.where(visit_count == 0, 0, value_sum / visit_count)
 
-    def add_exploration_noise(self, batch_index: torch.Tensor, node_index: torch.Tensor, exploration_fraction: float):
+    def add_exploration_noise(self, batch_index: torch.Tensor, children_index: torch.Tensor, exploration_fraction: float):
         concentration = torch.ones([len(batch_index), self.hparams.num_actions]).float() * self.hparams.dirichlet_alpha
         dist = torch.distributions.Dirichlet(concentration)
         noise = dist.sample()
-        noise = noise.to(self.hparams.device)
+        noise = noise.type(self.prior.dtype).to(self.hparams.device)
 
-        orig_priors = self.prior[batch_index].gather(1, node_index)
+        orig_priors = self.prior[batch_index].gather(1, children_index)
         priors = orig_priors * (1 - exploration_fraction) + noise * exploration_fraction
-        #self.prior.scatter_add_(1, node_index, priors)
-        self.prior.scatter_(1, node_index, priors)
+        #self.prior.scatter_add_(1, children_index, priors)
+        self.prior.scatter_(1, children_index, priors)
 
         # max_debug = 10
         # self.logger.info(f'exploration:\n'
         #                  f'orig_priors:\n{orig_priors[:max_debug]}\n'
         #                  f'priors:\n{priors[:max_debug]}\n'
-        #                   f'saved_priors:\n{self.prior.gather(1, node_index)[:max_debug]}\n')
+        #                   f'saved_priors:\n{self.prior.gather(1, children_index)[:max_debug]}\n')
 
-    def select_children(self, batch_index: torch.Tensor, node_index: torch.Tensor) -> torch.Tensor:
+    def select_children(self, depth_index: int, batch_index: torch.Tensor, node_index: torch.Tensor) -> torch.Tensor:
+        max_debug = 10
+
         children_index = self.children_index(batch_index, node_index)
-        #self.logger.debug(f'select_children: node_index: {node_index}, children_index:\n{children_index}')
+        # self.logger.debug(f'select_children: node_index: {node_index[:max_debug]}, children_index:\n{children_index}')
 
         ucb_scores = self.ucb_scores(batch_index, node_index, children_index)
-        # self.logger.info(f'select_children: usb_scores:\n{ucb_scores}')
+        # if depth_index < 2:
+        #     self.logger.info(f'depth: {depth_index}, select_children: ucb_scores:\n{ucb_scores[:max_debug]}')
         max_scores, _ = ucb_scores.max(1)
         not_max_indexes = ucb_scores < max_scores.unsqueeze(1)
 
         rnd = torch.rand(*ucb_scores.shape, requires_grad=False, device=ucb_scores.device)
         ucb_scores += rnd
 
-        ucb_scores[not_max_indexes] = 0
+        ucb_scores[not_max_indexes] = float('-inf')
         max_indexes = ucb_scores.argmax(dim=1)
 
-        # max_debug = 10
-        # self.logger.info(f'select_children: batch_index: {batch_index[:max_debug]}, '
-        #                  f'node_index: {node_index[:max_debug]}, '
-        #                  f'children_index:\n{children_index[:max_debug]}\n'
-        #                  f'ucb_scores:\n{ucb_scores[:max_debug]}\n'
-        #                  f'max_scores: {max_scores[:max_debug]}, '
-        #                  f'max_indexes: {max_indexes[:max_debug]}')
+        # if depth_index < 2:
+        #     self.logger.info(f'depth: {depth_index}, select_children: batch_index: {batch_index[:max_debug]}, '
+        #                      f'node_index: {node_index[:max_debug]}, '
+        #                      f'children_index:\n{children_index[:max_debug]}\n'
+        #                      f'max_masked_with_random_ucb_scores:\n{ucb_scores[:max_debug]}\n'
+        #                      f'max_scores_before_random:\n{max_scores[:max_debug]}, '
+        #                      f'max_indexes:\n{max_indexes[:max_debug]}')
 
         children_index = children_index.gather(1, max_indexes.unsqueeze(1)).squeeze(1)
         return max_indexes, children_index
@@ -216,7 +221,7 @@ class Tree:
     def ucb_scores(self, batch_index: torch.Tensor, parent_index: torch.Tensor, children_index: torch.Tensor) -> torch.Tensor:
         parent_visit_count = self.visit_count[batch_index, parent_index]
         children_visit_count = self.visit_count[batch_index].gather(1, children_index)
-        score = torch.log((parent_visit_count + self.hparams.c2 + 1) / self.hparams.c2) + self.hparams.c1
+        score = torch.log((parent_visit_count + self.hparams.pb_c_base + 1) / self.hparams.pb_c_base) + self.hparams.pb_c_init
         score *= torch.sqrt(parent_visit_count)
 
         score = score.unsqueeze(1) * torch.ones_like(children_visit_count)
@@ -226,10 +231,10 @@ class Tree:
         prior_score = score * children_prior
 
         children_value = self.value(batch_index, children_index)
-        if len(self.hparams.player_ids) != 1:
-            children_value *= -1
+        #if len(self.hparams.player_ids) != 1:
+        #    children_value *= -1
 
-        value_score = self.reward[batch_index].gather(1, children_index) + self.hparams.discount * children_value
+        value_score = self.reward[batch_index].gather(1, children_index) + self.hparams.ucb_value_score_discount * children_value
         score = prior_score + value_score
 
         # max_debug = 10
@@ -244,35 +249,44 @@ class Tree:
         return score
 
     def backpropagate(self, player_id: torch.Tensor, search_path: torch.Tensor, episode_len: torch.Tensor, value: torch.Tensor):
-        for current_episode_len in torch.arange(episode_len.max(), -1, step=-1).to(self.hparams.device):
-            batch_index = torch.arange(self.hparams.batch_size)[episode_len >= current_episode_len].to(self.hparams.device)
+        batch_index_full = torch.arange(self.hparams.batch_size)
+        for current_episode_len in torch.arange(episode_len.max()-1, -1, step=-1).to(self.hparams.device):
+            batch_index = batch_index_full[episode_len >= current_episode_len].to(self.hparams.device)
 
-            node_index = search_path[batch_index, current_episode_len]
-            node_multiplier = torch.where(self.player_id[batch_index, node_index] == player_id[batch_index], 1., -1.)
+            node_index = search_path[batch_index, current_episode_len+1]
+
+            stored_player_id_for_step = self.player_id[batch_index, node_index]
+            game_player_id_for_step = player_id[batch_index, current_episode_len]
+            node_multiplier = torch.where(stored_player_id_for_step == game_player_id_for_step, 1., -1.).type(self.reward.dtype)
 
             # debug_max = 10
             # self.logger.info(f'backpropagate: '
-            #                  f'self.player_id: {self.player_id[batch_index, node_index][:debug_max]}, '
-            #                  f'player_id: {player_id[batch_index][:debug_max]}, '
-            #                  f'node_index: {node_index.shape}, node_index:\n{node_index[:debug_max]}')
+            #                  f'current_episode_len: {current_episode_len}/{episode_len.max()}/{episode_len[:debug_max]}\n'
+            #                  f'self.player_id        : {self.player_id[batch_index, node_index][:debug_max]}\n'
+            #                  f'self.player_id batched: {self.player_id[batch_index, node_index][:debug_max]}\n'
+            #                  f'     player_id        : {player_id[batch_index][:debug_max]}\n'
+            #                  f'node_index: {node_index.shape}\n{node_index[:debug_max]}')
 
-            # self.logger.debug(f'backpropagate: batch_index: {batch_index.shape}, batch_index: {batch_index[:debug_max]}, '
-            #                  f'current_episode_len: {current_episode_len}/{episode_len.max()}/{episode_len[:debug_max]}, '
-            #                  f'value: {value[batch_index].shape}/{value.shape}, '
-            #                  f'value: {value[batch_index][:debug_max]}/{value[:debug_max]}')
+            # self.logger.info(f'backpropagate: batch_index: {batch_index.shape}\n{batch_index[:debug_max]}\n'
+            #                  f'current_episode_len: {current_episode_len}/{episode_len.max()}/{episode_len[:debug_max]}\n'
+            #                  f'value: {value[batch_index].shape}/{value.shape}\n{value[batch_index][:debug_max]}/{value[:debug_max]}')
 
             self.value_sum[batch_index, node_index] += value[batch_index] * node_multiplier
             # self.logger.info(f'backpropagate: node_multiplier: {node_multiplier}, value_sum:\n{self.value_sum[batch_index]}')
             self.visit_count[batch_index, node_index] += 1
 
-            value[batch_index] = self.reward[batch_index, node_index] * node_multiplier + self.hparams.discount * value[batch_index]
+            value[batch_index] = self.reward[batch_index, node_index] * node_multiplier + self.hparams.value_discount * value[batch_index]
             self.min_max_stats.update(value)
+
+        self.value_sum[:, 0] += value
+        self.visit_count[:, 0] += 1
+
 
     def _store_states(self, search_path: torch.Tensor, episode_len: torch.Tensor, hidden_states: torch.Tensor):
         pass
 
     def _load_states(self, search_path: torch.Tensor, episode_len: torch.Tensor) -> torch.Tensor:
-        hidden_states = torch.zeros(self.hparams.batch_size, 16, *self.hparams.state_shape[1:]).to(self.hparams.device)
+        hidden_states = torch.zeros(self.hparams.batch_size, 12, *self.hparams.state_shape).to(self.hparams.device)
         return hidden_states
 
     def store_states(self, search_path: torch.Tensor, episode_len: torch.Tensor, hidden_states: torch.Tensor):
@@ -298,20 +312,21 @@ class Tree:
         hidden_states = torch.stack(hidden_states, 0).to(self.hparams.device)
         return hidden_states
 
-    def run_one_simulation(self):
+    def run_one_simulation(self, initial_player_id: torch.Tensor):
         search_path = torch.zeros(self.hparams.batch_size, self.hparams.max_episode_len+1).long().to(self.hparams.device)
         actions = torch.zeros(self.hparams.batch_size, self.hparams.max_episode_len).long().to(self.hparams.device)
-        episode_len = torch.zeros(self.hparams.batch_size).long().to(self.hparams.device)
-        player_id = torch.ones(self.hparams.batch_size).long().to(self.hparams.device) * self.hparams.player_ids[0]
+        player_id = torch.zeros(self.hparams.batch_size, self.hparams.max_episode_len, dtype=torch.uint8).to(self.hparams.device)
+        episode_len = torch.zeros(self.hparams.batch_size, dtype=torch.uint8, device=self.hparams.device)
         max_debug = 10
 
         batch_index = torch.arange(self.hparams.batch_size).to(self.hparams.device)
         node_index = torch.zeros(self.hparams.batch_size).long().to(self.hparams.device)
 
         search_path[batch_index, 0] = node_index
+        step_player_id = initial_player_id.detach().clone()
 
         for depth_index in range(0, self.hparams.max_episode_len):
-            action_index, children_index = self.select_children(batch_index, node_index)
+            action_index, children_index = self.select_children(depth_index, batch_index, node_index)
 
             # self.logger.info(f'depth: {depth_index}, '
             #                  f'player_id: {player_id[batch_index][:max_debug]}, '
@@ -325,6 +340,7 @@ class Tree:
             search_path[batch_index, depth_index+1] = children_index.detach().clone()
             actions[batch_index, depth_index] = action_index.detach().clone()
             episode_len[batch_index] += 1
+            player_id[batch_index, depth_index] = step_player_id[batch_index].detach().clone()
 
 
             expanded_index = self.expanded[batch_index, children_index] == True
@@ -335,17 +351,20 @@ class Tree:
             if len(node_index) == 0:
                 break
 
-            if depth_index >= 1:
-                player_id[batch_index] = player_id_change(self.hparams, player_id[batch_index])
-
+            step_player_id = player_id_change(self.hparams, step_player_id)
 
         try:
-            batch_index = torch.arange(self.hparams.batch_size).to(self.hparams.device)
+            episode_len = episode_len.long()
+            batch_index = torch.arange(self.hparams.batch_size, device=self.hparams.device)
 
             last_episode = episode_len - 1
             last_episode = last_episode.unsqueeze(1)
 
             hidden_states = self.load_states(search_path, episode_len)
+            hidden_states_sum = hidden_states.sum([1, 2, 3])
+            hidden_states_non_zero = torch.count_nonzero(hidden_states_sum, 0)
+            if hidden_states_non_zero != len(hidden_states):
+                self.logger.info(f'batch: {len(hidden_states)}, non-zero: {hidden_states_non_zero}, ratio: {hidden_states_non_zero/len(hidden_states)}')
 
             last_actions = actions.gather(1, last_episode).squeeze(1)
             out = self.inference.recurrent(hidden_states, last_actions)
@@ -353,6 +372,7 @@ class Tree:
 
 
             parent_index = search_path.gather(1, episode_len.unsqueeze(1)).squeeze(1)
+            last_player_id = player_id.gather(1, last_episode).squeeze(1)
 
             # max_debug = 10
             # self.logger.info(f'search_path: {search_path.shape}\n{search_path[:max_debug, :episode_len.max()+1]}')
@@ -360,14 +380,14 @@ class Tree:
             # self.logger.info(f'episode_len: {episode_len[:max_debug]}')
             # self.logger.info(f'parent_index: {parent_index[:max_debug]}')
 
-            self.expand(player_id, batch_index, parent_index, out.policy_logits)
+            self.expand(last_player_id, batch_index, parent_index, out.policy_logits.type(torch.bfloat16))
 
-            self.backpropagate(player_id, search_path, episode_len, out.value)
+            self.backpropagate(player_id, search_path, episode_len, out.value.type(torch.bfloat16))
         except:
-            self.logger.error(f'search_path: {search_path.shape}\n{search_path[:max_debug, :episode_len.max()+1]}')
-            self.logger.error(f'actions: {actions.shape}\n{actions[:max_debug, :episode_len.max()]}')
-            self.logger.error(f'episode_len: {episode_len[:max_debug]}')
-            self.logger.error(f'player_id: {player_id[:max_debug]}')
+            # self.logger.error(f'search_path: {search_path.shape}\n{search_path[:max_debug, :episode_len.max()+1]}')
+            # self.logger.error(f'actions: {actions.shape}\n{actions[:max_debug, :episode_len.max()]}')
+            # self.logger.error(f'episode_len: {episode_len[:max_debug]}')
+            # self.logger.error(f'player_id: {player_id[:max_debug]}')
             raise
 
         return search_path, episode_len

muzero.py
@@ -112,17 +112,11 @@ class Trainer:
         self.grpc_server.wait_for_termination()
 
     def training_forward_one_game(self, epoch: int, game_stat: simulation.GameStats):
-        start_pos = []
-        for i, max_episode_len in enumerate(game_stat.episode_len):
-            max_start_pos = max_episode_len - self.hparams.num_unroll_steps
-            if max_start_pos <= 0:
-                pos = torch.tensor([0]).long()
-            else:
-                pos = torch.randint(low=0, high=max_start_pos.item(), size=(1,))
-
-            start_pos.append(pos)
-
-        start_pos = torch.cat(start_pos).to(self.hparams.device)
+        start_pos = torch.randint(low=0, high=game_stat.episode_len.max(), size=(len(game_stat.episode_len),)).to(self.hparams.device)
+        start_pos -= self.hparams.num_unroll_steps
+        start_pos = torch.where(start_pos <= 0, 0, start_pos)
+        start_pos = torch.where(start_pos+self.hparams.num_unroll_steps>=game_stat.episode_len, 0, start_pos)
+        #self.logger.info(f'epoch: {epoch}: start_pos: {start_pos.shape}: {start_pos[:10]}, episode_len: {game_stat.episode_len[:10]}')
 
         self.summary_writer.add_scalars('train/episode_len', {
             'mean': game_stat.episode_len.float().mean(),
@@ -202,33 +196,35 @@ class Trainer:
 
         return total_loss, train_examples
 
+    def training_step_for_one_game(self, epoch: int, game_stat: simulation.GameStats):
+        self.inference.zero_grad()
+        for opt in self.optimizers:
+            opt.zero_grad()
+
+        total_loss, total_train_examples = self.training_forward_one_game(epoch, game_stat)
+
+        total_loss.backward()
+
+        for opt in self.optimizers:
+            opt.step()
+
+        self.summary_writer.add_scalar('train/total_loss', total_loss, self.global_step)
+        self.summary_writer.add_scalar('train/samples', total_train_examples, self.global_step)
+
+        self.global_step += 1
+        self.save_muzero_server_weights()
+
     def training_step(self, epoch: int):
         self.inference.train(True)
 
-        all_keys = sorted(list(self.all_games.keys()))
-        all_games = self.all_games
-        self.all_games = defaultdict(list)
-        for key in all_keys:
-            for game_stat in all_games[key]:
-                self.inference.zero_grad()
-                for opt in self.optimizers:
-                    opt.zero_grad()
-
-                total_loss, total_train_examples = self.training_forward_one_game(epoch, game_stat)
-
-                total_loss.backward()
-
-                for opt in self.optimizers:
-                    opt.step()
-
-                self.summary_writer.add_scalar('train/total_loss', total_loss, self.global_step)
-                self.summary_writer.add_scalar('train/samples', total_train_examples, self.global_step)
-
-
-                self.global_step += 1
-                self.save_muzero_server_weights()
-
         self.move_games()
+        all_keys = sorted(list(self.all_games.keys()))
+        all_games = []
+        for key in all_keys:
+            all_games += self.all_games[key]
+
+        game_stat = np.random.choice(all_games)
+        self.training_step_for_one_game(epoch, game_stat)
 
     def move_games(self) -> int:
         new_games = self.muzero_server.move_games()
@@ -272,8 +268,6 @@ class Trainer:
         self.muzero_server.update_weights(self.global_step, meta)
 
     def run_training(self, epoch: int):
-        self.summary_writer.add_scalar('train/epoch', epoch, self.global_step)
-
         while self.move_games() == 0:
             time.sleep(1)
 

networks.py
@@ -79,19 +79,19 @@ class ConnectXParams(NetworkParams):
 
 class TicTacToeParams(NetworkParams):
     kernel_size: int = 3
-    hidden_size: int = 12
+    hidden_size: int = 16
 
-    repr_conv_res_num_features: int = 12
-    repr_conv_num_blocks: int = 2
+    repr_conv_res_num_features: int = 24
+    repr_conv_num_blocks: int = 3
 
-    pred_conv_res_num_features: int = 12
-    pred_conv_num_blocks: int = 2
-    pred_hidden_linear_layers: List[int] = [32, 32]
-    num_actions: int = 0
+    pred_conv_res_num_features: int = 24
+    pred_conv_num_blocks: int = 3
+    pred_hidden_linear_layers: List[int] = [128, 128]
+    num_actions: int = 9
 
-    dyn_conv_res_num_features: int = 12
-    dyn_conv_num_blocks: int = 2
-    dyn_reward_linear_layers: List[int] = [32, 32]
+    dyn_conv_res_num_features: int = 24
+    dyn_conv_num_blocks: int = 3
+    dyn_reward_linear_layers: List[int] = [128, 128]
 
     activation_str: str = 'LeakyReLU'
     activation_args: List = []
@@ -354,11 +354,11 @@ class Inference(GenericInference):
 
     def recurrent(self, hidden_states: torch.Tensor, actions: torch.Tensor) -> NetworkOutput:
         batch_size = len(hidden_states)
-        policy_logits, values = self.prediction(hidden_states)
         actions_exp = F.one_hot(actions, self.hparams.num_actions)
         actions_exp = actions_exp.view(batch_size, *self.hparams.state_shape)
         actions_exp = actions_exp.unsqueeze(1)
         inputs = torch.cat([hidden_states, actions_exp], 1)
         new_hidden_states, rewards = self.dynamic(inputs)
+        policy_logits, values = self.prediction(new_hidden_states)
         #self.logger.info(f'inference: recurrent: hidden_states: {hidden_states.shape}, policy_logits: {policy_logits.shape}, rewards: {rewards.shape}, values: {values.shape}')
         return NetworkOutput(reward=rewards, hidden_state=new_hidden_states, policy_logits=policy_logits, value=values)

simulation.py
@@ -5,6 +5,7 @@ import logging
 from time import perf_counter
 
 import torch
+from torch.utils.tensorboard import SummaryWriter
 
 #import connectx_impl
 import tictactoe_impl
@@ -26,18 +27,27 @@ def roll_by_gather(mat, dim, shifts: torch.LongTensor):
         return torch.gather(mat, 1, arange2)
 
 class GameStats:
+    episode_len: torch.Tensor
+    rewards: torch.Tensor
+    root_values: torch.Tensor
+    children_visits: torch.Tensor
+    actions: torch.Tensor
+    player_ids: torch.Tensor
+    dones: torch.Tensor
+    game_states: torch.Tensor
+
     def __init__(self, hparams: Hparams, logger: logging.Logger):
         self.logger = logger
         self.hparams = hparams
 
-        self.episode_len = torch.zeros(hparams.batch_size, dtype=torch.int64, device=hparams.device)
-        self.rewards = torch.zeros(hparams.batch_size, hparams.max_episode_len, dtype=hparams.dtype, device=hparams.device)
-        self.root_values = torch.zeros(hparams.batch_size, hparams.max_episode_len, dtype=hparams.dtype, device=hparams.device)
-        self.children_visits = torch.zeros(hparams.batch_size, hparams.num_actions, hparams.max_episode_len, dtype=hparams.dtype, device=hparams.device)
-        self.actions = torch.zeros(hparams.batch_size, hparams.max_episode_len, dtype=torch.int64, device=hparams.device)
-        self.player_ids = torch.zeros(hparams.batch_size, hparams.max_episode_len, dtype=torch.int64, device=hparams.device)
+        self.episode_len = torch.zeros(hparams.batch_size, dtype=torch.uint8, device=hparams.device)
+        self.rewards = torch.zeros(hparams.batch_size, hparams.max_episode_len, dtype=torch.bfloat16, device=hparams.device)
+        self.root_values = torch.zeros(hparams.batch_size, hparams.max_episode_len, dtype=torch.bfloat16, device=hparams.device)
+        self.children_visits = torch.zeros(hparams.batch_size, hparams.num_actions, hparams.max_episode_len, dtype=torch.bfloat16, device=hparams.device)
+        self.actions = torch.zeros(hparams.batch_size, hparams.max_episode_len, dtype=torch.uint8, device=hparams.device)
+        self.player_ids = torch.zeros(hparams.batch_size, hparams.max_episode_len, dtype=torch.uint8, device=hparams.device)
         self.dones = torch.zeros(hparams.batch_size, dtype=torch.bool, device=hparams.device)
-        self.game_states = torch.zeros([hparams.batch_size, hparams.max_episode_len] + hparams.state_shape, dtype=hparams.dtype, device=hparams.device)
+        self.game_states = torch.zeros([hparams.batch_size, hparams.max_episode_len] + hparams.state_shape, dtype=torch.uint8, device=hparams.device)
 
         self.stored_tensors = {
             'rewards': self.rewards,
@@ -66,7 +76,9 @@ class GameStats:
         return self.episode_len.sum().item()
 
     def append(self, index: torch.Tensor, tensors_dict: Dict[str, torch.Tensor]):
-        episode_len = self.episode_len[index]
+        episode_len = self.episode_len[index].long()
+        index = index.long()
+
         for key, value in tensors_dict.items():
             if not key in self.stored_tensors:
                 msg = f'invalid key: {key}, tensor shape: {value.shape}, available keys: {list(self.stored_tensors.keys())}'
@@ -93,7 +105,7 @@ class GameStats:
         sample_len = torch.zeros(len(start_index), dtype=torch.int64, device=self.hparams.device)
 
         batch_index = torch.arange(len(start_index), dtype=torch.int64, device=self.hparams.device)
-        game_states = self.game_states[batch_index, start_index]
+        game_states = self.game_states[batch_index, start_index].float()
 
         if start_index.device != self.episode_len.device:
             msg = f'start_index: {start_index.device}, self.episode_len: {self.episode_len.device}'
@@ -110,21 +122,21 @@ class GameStats:
                 exit(-1)
 
             bootstrap_update_index = bootstrap_index < self.episode_len
-            valid_batch_index = batch_index[bootstrap_update_index]
 
             # self.logger.info(f'{unroll_index}: '
             #              f'start_index: {start_index.cpu().numpy()}, '
             #              f'current_index: {current_index.cpu().numpy()}, '
             #              f'bootstrap_index: {bootstrap_index.cpu().numpy()}, '
             #              f'bootstrap_update_index: {bootstrap_update_index.cpu().numpy()}/{bootstrap_update_index.shape}, '
-            #              f'valid_batch_index: {valid_batch_index.cpu().numpy()}'
             #              )
-            values = torch.zeros(len(self.root_values), dtype=self.root_values.dtype, device=self.hparams.device)
+            values = torch.zeros(len(self.root_values), device=self.hparams.device).float()
             if bootstrap_update_index.sum() > 0:
                 #self.logger.info(f'bootstrap_update_index: {bootstrap_update_index}')
-                values[bootstrap_update_index] = self.root_values[valid_batch_index, bootstrap_index] * self.hparams.discount ** self.hparams.td_steps
+                valid_batch_index = batch_index[bootstrap_update_index]
+                last_discount = self.hparams.value_discount ** self.hparams.td_steps
+                values[bootstrap_update_index] = self.root_values[valid_batch_index, bootstrap_index].float() * last_discount
 
-            discount_mult = torch.logspace(0, self.hparams.td_steps, self.rewards.shape[1], base=self.hparams.discount).to(self.hparams.device)
+            discount_mult = torch.logspace(0, self.hparams.td_steps, self.rewards.shape[1], base=self.hparams.value_discount).to(self.hparams.device)
             if discount_mult.device != start_index.device:
                 msg = f'1 start_index: {start_index.device}, discount_mutl: {discount_mult.device}, hparams.device: {self.hparams.device}'
                 self.logger.critical(msg)
@@ -152,7 +164,7 @@ class GameStats:
             masked_rewards = torch.where(all_rewards_index < current_index.unsqueeze(1), 0, self.rewards)
             masked_rewards = torch.where(all_rewards_index >= last_index.unsqueeze(1), 0, masked_rewards)
 
-            discounted_rewards = self.rewards * discount_mult
+            discounted_rewards = self.rewards.float() * discount_mult
             discounted_rewards = discounted_rewards.sum(1)
             values += discounted_rewards
 
@@ -160,12 +172,12 @@ class GameStats:
 
             current_valid_index = current_index[valid_index]
             target_values[valid_index, unroll_index] = values[valid_index]
-            target_children_visits[valid_index, :, unroll_index] = self.children_visits[valid_index, :, current_valid_index]
+            target_children_visits[valid_index, :, unroll_index] = self.children_visits[valid_index, :, current_valid_index].float()
 
             if unroll_index > 0:
-                target_last_rewards[valid_index, unroll_index] = self.rewards[valid_index, current_valid_index-1]
-            taken_actions[valid_index, unroll_index] = self.actions[valid_index, current_valid_index]
-            player_ids[valid_index, unroll_index] = self.player_ids[valid_index, current_valid_index]
+                target_last_rewards[valid_index, unroll_index] = self.rewards[valid_index, current_valid_index-1].float()
+            taken_actions[valid_index, unroll_index] = self.actions[valid_index, current_valid_index].long()
+            player_ids[valid_index, unroll_index] = self.player_ids[valid_index, current_valid_index].long()
             sample_len[valid_index] += 1
 
         return {
@@ -180,10 +192,13 @@ class GameStats:
 
 
 class Train:
-    def __init__(self, hparams: Hparams, inference: Inference, logger: logging.Logger):
+    def __init__(self, hparams: Hparams, inference: Inference, logger: logging.Logger, summary_writer: SummaryWriter, summary_prefix: str):
         self.hparams = hparams
         self.logger = logger
         self.inference = inference
+        self.summary_writer = summary_writer
+        self.summary_prefix = summary_prefix
+        self.summary_step = 0
 
         self.num_train_steps = 0
         self.game_stats = GameStats(hparams, self.logger)
@@ -199,21 +214,22 @@ class Train:
 
         out = self.inference.initial(initial_player_id, initial_game_states)
 
-        episode_len = torch.ones(len(node_index)).long().to(self.hparams.device)
+        episode_len = torch.ones(len(node_index), dtype=torch.uint8).to(self.hparams.device)
         search_path = torch.zeros(len(node_index), 1).long().to(self.hparams.device)
 
         tree.store_states(search_path, episode_len, out.hidden_state)
 
-        tree.expand(initial_player_id, batch_index, node_index, out.policy_logits)
+        tree.expand(initial_player_id, batch_index, node_index, out.policy_logits.type(torch.bfloat16))
         tree.visit_count[batch_index, node_index] = 1
-        tree.value_sum[batch_index, node_index] += out.value
+        tree.value_sum[batch_index, node_index] += out.value.type(torch.bfloat16)
 
         if self.hparams.add_exploration_noise:
             children_index = tree.children_index(batch_index, node_index)
             tree.add_exploration_noise(batch_index, children_index, self.hparams.exploration_fraction)
 
         for _ in range(self.hparams.num_simulations):
-            search_path, episode_len = tree.run_one_simulation()
+            search_path, episode_len = tree.run_one_simulation(initial_player_id)
+
         simulation_time = perf_counter() - start_simulation_time
         one_sim_ms = int(simulation_time / self.hparams.num_simulations * 1000)
 
@@ -225,26 +241,29 @@ class Train:
 
         node_index = torch.zeros(batch_size).long().to(self.hparams.device)
         children_index = tree.children_index(batch_index, node_index)
-        children_visit_counts = tree.visit_count[batch_index].gather(1, children_index)
+        children_visit_counts = tree.visit_count[batch_index].gather(1, children_index).float()
         children_sum_visits = children_visit_counts.sum(1)
         children_visits = children_visit_counts / children_sum_visits.unsqueeze(1)
         root_values = tree.value(batch_index, node_index.unsqueeze(1)).squeeze(1)
 
-        if self.num_train_steps >= 26:
+        if self.num_train_steps >= 6:
             actions = torch.argmax(children_visit_counts, 1)
         else:
             temperature = 1.0 # play according to softmax distribution
 
-            dist = torch.pow(children_visit_counts.float(), 1 / temperature)
+            dist = torch.pow(children_visit_counts, 1 / temperature)
             actions = torch.multinomial(dist, 1)
             actions = actions.squeeze(1)
 
+        actions = actions.type(torch.uint8)
+        children_visits = children_visits.type(torch.bfloat16)
         # max_debug = 10
         # self.logger.info(f'train_steps: {self.num_train_steps}, '
         #                  f'children_index:\n{children_index[:max_debug]}\n'
         #                  f'children_visit_counts:\n{children_visit_counts[:max_debug]}\n'
         #                  f'children_sum_visits:\n{children_sum_visits[:max_debug]}\n'
         #                  f'children_visits:\n{children_visits[:max_debug]}\n'
+        #                  f'root_values: {root_values.shape}\n{root_values[:max_debug]}\n'
         #                  f'actions:\n{actions[:max_debug]}')
         return actions, children_visits, root_values
 
@@ -254,8 +273,8 @@ class Train:
 def run_single_game(hparams: Hparams, train: Train, num_steps: int):
     #game_hparams = connectx_impl.Hparams()
     game_hparams = tictactoe_impl.Hparams()
-    game_states = torch.zeros(hparams.batch_size, *hparams.state_shape).float().to(hparams.device)
-    player_ids = torch.ones(hparams.batch_size, device=hparams.device).long() * hparams.player_ids[0]
+    game_states = torch.zeros(hparams.batch_size, *hparams.state_shape, dtype=torch.uint8).to(hparams.device)
+    player_ids = torch.ones(hparams.batch_size, device=hparams.device, dtype=torch.uint8) * hparams.player_ids[0]
 
     active_games_index = torch.arange(hparams.batch_size).long().to(hparams.device)
 
@@ -265,7 +284,7 @@ def run_single_game(hparams: Hparams, train: Train, num_steps: int):
         actions, children_visits, root_values = train.run_simulations(active_player_ids, active_game_states)
         #new_game_states, rewards, dones = connectx_impl.step_games(game_hparams, active_game_states, active_player_ids, actions)
         new_game_states, rewards, dones = tictactoe_impl.step_games(game_hparams, active_game_states, active_player_ids, actions)
-        game_states[active_games_index] = new_game_states
+        game_states[active_games_index] = new_game_states.detach().clone()
 
         train.game_stats.append(active_games_index, {
             'children_visits': children_visits,

tictactoe_impl.py
@@ -5,16 +5,16 @@ class Hparams:
     columns: int = 3
     inarow: int = 3
     default_reward: float = 0
-    invalid_action_reward: float = -1
+    invalid_action_reward: float = -10
 
     def __init__(self):
         self.rows = 3
         self.columns = 3
         self.inarow = 3
         self.default_reward = 0
-        self.invalid_action_reward = -1
+        self.invalid_action_reward = -10
 
-#@torch.jit.script
+@torch.jit.script
 def check_reward(hparams: Hparams, games: torch.Tensor, player_id: torch.Tensor):
     player_id = player_id.unsqueeze(1).unsqueeze(1)
     columns_end = hparams.columns - (hparams.inarow - 1)
@@ -56,16 +56,15 @@ def check_reward(hparams: Hparams, games: torch.Tensor, player_id: torch.Tensor)
                 dones[idx[win_idx]] = True
                 idx = idx[torch.logical_not(win_idx)]
 
-    rewards = torch.where(dones, 1.0, hparams.default_reward)
+    rewards = torch.where(dones, 1.0, hparams.default_reward).type(torch.bfloat16)
 
     return rewards, dones
 
-#@torch.jit.script
+@torch.jit.script
 def step_games(hparams: Hparams, games: torch.Tensor, player_id: torch.Tensor, actions: torch.Tensor):
-    player_id = player_id.float()
-
     num_games = len(games)
     flat_games = games.view([num_games, -1])
+    actions = actions.long()
     dst_actions = flat_games[torch.arange(num_games, dtype=torch.int64), actions]
 
     invalid_action_index_batch = dst_actions != 0
@@ -75,8 +74,12 @@ def step_games(hparams: Hparams, games: torch.Tensor, player_id: torch.Tensor, a
     flat_games[good_action_index_batch, good_actions_index] = player_id[good_action_index_batch]
 
     rewards, dones = check_reward(hparams, games, player_id)
-    rewards[invalid_action_index_batch] = torch.tensor(hparams.invalid_action_reward, dtype=torch.float32)
+    rewards[invalid_action_index_batch] = torch.tensor(hparams.invalid_action_reward, dtype=torch.bfloat16)
     dones[invalid_action_index_batch] = True
+
+    num_zeros = torch.count_nonzero(flat_games == 0, -1)
+    finished_index = num_zeros == 0
+    dones[finished_index] = True
 
     max_debug = 5
     # print(f'invalid_actions_index: {invalid_action_index_batch[:max_debug]}')