d1b571ba2144fd98d7f5756fb530b04c41cb80ee
Evgeniy Polyakov <zbr@ioremap.net>
2023-02-15 22:09:29 +0000

    Added offline/online training modes and (unused) gradient analysis code, added various tensorboard logs, start using GameState, use HuberLoss instead of L2

--- muzero.py
+++ muzero.py
@@ -20,6 +20,9 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.tensorboard import SummaryWriter
 
+
+from matplotlib.lines import Line2D
+
 torch.backends.cuda.matmul.allow_tf32 = True
 
 from evaluate_score import EvaluationDataset
@@ -70,7 +73,7 @@ class Trainer:
         if os.path.exists(tensorboard_log_dir) and len(os.listdir(tensorboard_log_dir)) > 0:
             first_run = False
 
-        self.summary_writer = SummaryWriter(log_dir=tensorboard_log_dir)
+        self.summary_writer = SummaryWriter(log_dir=tensorboard_log_dir, flush_secs=1)
         self.global_step = 0
 
         self.inference = networks.Inference(self.game_ctl, logger)
@@ -81,7 +84,8 @@ class Trainer:
         self.optimizers = [self.representation_opt, self.prediction_opt, self.dynamic_opt]
 
         self.ce_loss = nn.CrossEntropyLoss(reduction='none')
-        self.scalar_loss = nn.MSELoss(reduction='none')
+        #self.scalar_loss = nn.MSELoss(reduction='none')
+        self.scalar_loss = nn.HuberLoss(delta=1.1, reduction='none')
 
         self.all_games: Dict[int, List[simulation.GameStats]] = defaultdict(list)
 
@@ -105,28 +109,50 @@ class Trainer:
         # children_visit_counts: [B, Nactions]
         children_visits_sum = children_visit_counts_for_step.sum(1, keepdim=True)
         action_probs = children_visit_counts_for_step / children_visits_sum
+        pred_probs = F.softmax(policy_logits, 1)
         loss = self.ce_loss(policy_logits, action_probs)
+        return loss
+
+        self.logger.info(f'policy_logits: {policy_logits[:10]}')
+
+        action_log_probs = action_probs.log()
+        pred_log_probs = pred_probs.log()
+
+        loss_raw = (pred_log_probs - action_log_probs).exp()
+        loss_clipped = loss_raw.clamp(0.9, 1.1)
+        loss = torch.min(loss_raw, loss_clipped)
+        loss = loss.mean(1)
+
+        # max_debug = 10
+        # self.logger.info(f'action_probs:\n{action_probs[:max_debug]}\n'
+        #                  f'pred_probs:\n{pred_probs[:max_debug]}\n'
+        #                  f'action_log_probs:\n{action_log_probs[:max_debug]}\n'
+        #                  f'pred_log_probs:\n{pred_log_probs[:max_debug]}\n'
+        #                  f'loss_raw:\n{loss_raw[:max_debug]}\n'
+        #                  f'loss_clipped:\n{loss_clipped[:max_debug]}')
         return loss
 
     def training_step(self, sample: simulation.TrainElement):
         sample_len_max = sample.sample_len.max().item()
         sample.sample_len = sample.sample_len.float()
 
-        out = self.inference.initial(sample.player_ids[:, 0], sample.game_states)
+        out = self.inference.initial(sample.initial_game_state)
 
         policy_loss = self.policy_loss(out.policy_logits, sample.children_visits[:, :, 0])
-        value_loss = self.scalar_loss(out.value, sample.values[:, 0])
+        value_loss = self.scalar_loss(out.value.squeeze(1), sample.values[:, 0])
 
         iteration_loss = policy_loss + value_loss
         total_loss_mean = torch.mean(iteration_loss)
 
         policy_loss_mean = policy_loss.mean()
         value_loss_mean = value_loss.mean()
-        reward_loss_mean = 0
+        value_non_zero_loss_mean = value_loss[sample.start_index != 0].mean()
+        #reward_loss_mean = 0
 
         self.summary_writer.add_scalars('train/initial_losses', {
                 'policy': policy_loss_mean,
                 'value': value_loss_mean,
+                'value_non_zero': value_non_zero_loss_mean,
                 'total': total_loss_mean,
         }, self.global_step)
 
@@ -152,43 +178,140 @@ class Trainer:
             actions = sample.actions[batch_index]
             values = sample.values[batch_index]
             children_visits = sample.children_visits[batch_index]
-            rewards = sample.rewards[batch_index]
+            #rewards = sample.rewards[batch_index]
 
             hidden_states = out.hidden_state[len_idx]
 
-            out = self.inference.recurrent(hidden_states, actions[:, step_idx-1])
+            last_actions = actions[:, step_idx-1]
+            out = self.inference.recurrent(hidden_states, last_actions)
 
             # scale = torch.ones_like(hidden_states, device=out.hidden_state.device) * 0.5
             # hidden_states = scale_gradient(hidden_states, scale)
 
             policy_loss = self.policy_loss(out.policy_logits, children_visits[:, :, step_idx])
-            value_loss = self.scalar_loss(out.value, values[:, step_idx])
-            reward_loss = self.scalar_loss(out.reward, rewards[:, step_idx-1])
+            value_loss = self.scalar_loss(out.value.squeeze(1), values[:, step_idx])
+            #reward_loss = self.scalar_loss(out.reward.squeeze(1), rewards[:, step_idx-1])
 
-            iteration_loss = policy_loss + value_loss + reward_loss
+            #iteration_loss = policy_loss + value_loss + reward_loss
+            iteration_loss = policy_loss + value_loss
             iteration_loss = scale_gradient(iteration_loss, 1/sample_len)
 
             total_loss_mean += torch.mean(iteration_loss)
             policy_loss_mean += policy_loss.mean()
             value_loss_mean += value_loss.mean()
-            reward_loss_mean += reward_loss.mean()
+            #reward_loss_mean += reward_loss.mean()
 
         self.summary_writer.add_scalars('train/final_losses', {
                 'policy': policy_loss_mean,
-                'reward': reward_loss_mean,
+                #'reward': reward_loss_mean,
                 'value': value_loss_mean,
                 'total': total_loss_mean,
         }, self.global_step)
 
         return total_loss_mean
 
-    def run_training(self):
+    def plot_grad_flow(self, model):
+        '''Plots the gradients flowing through different layers in the net during training.
+        Can be used for checking for possible gradient vanishing / exploding problems.
+
+        Usage: Plug this function in Trainer class after loss.backwards() as
+        "plot_grad_flow(self.model.named_parameters())" to visualize the gradient flow'''
+
+        ave_grads = []
+        max_grads= []
+        layers = []
+        for n, p in model.named_parameters:
+            if(p.requires_grad) and ("bias" not in n):
+                layers.append(n)
+                ave_grads.append(p.grad.abs().mean())
+                max_grads.append(p.grad.abs().max())
+        plt.bar(np.arange(len(max_grads)), max_grads, alpha=0.1, lw=1, color="c")
+        plt.bar(np.arange(len(max_grads)), ave_grads, alpha=0.1, lw=1, color="b")
+        plt.hlines(0, 0, len(ave_grads)+1, lw=2, color="k" )
+        plt.xticks(range(0,len(ave_grads), 1), layers, rotation="vertical")
+        plt.xlim(left=0, right=len(ave_grads))
+        plt.ylim(bottom = -0.001, top=0.02) # zoom in on the lower gradient regions
+        plt.xlabel("Layers")
+        plt.ylabel("average gradient")
+        plt.title("Gradient flow")
+        plt.grid(True)
+        plt.legend([Line2D([0], [0], color="c", lw=4),
+                    Line2D([0], [0], color="b", lw=4),
+                    Line2D([0], [0], color="k", lw=4)], ['max-gradient', 'mean-gradient', 'zero-gradient'])
+
+
+    def plot_grad_tensorboard(self, model):
+        #... your learning loop
+        _limits = np.array([float(i) for i in range(len(gradmean))])
+        _num = len(gradmean)
+        writer.add_histogram_raw(tag=netname+"/abs_mean", min=0.0, max=0.3, num=_num,
+                                 sum=gradmean.sum(), sum_squares=np.power(gradmean, 2).sum(), bucket_limits=_limits,
+                                 bucket_counts=gradmean, global_step=self.global_step)
+        # where gradmean is np.abs(p.grad.clone().detach().cpu().numpy()).mean()
+        # _limits is the x axis, the layers
+        # and
+        _mean = {}
+        for i, name in enumerate(layers):
+            _mean[name] = gradmean[i]
+        self.writer.add_scalars(netname+"/abs_mean", _mean, global_step=self.global_step)
+
+    def run_training_online(self):
+        self.replay_buffer.truncate(max_generation=self.global_step)
+        while self.replay_buffer.num_games() < 10:
+            self.replay_buffer.truncate(max_generation=self.global_step)
+
+            time.sleep(1)
+
+        self.inference.train(True)
+
+        all_games = self.replay_buffer.flatten_games()
+
+        for _ in range(self.hparams.num_training_steps):
+            start_time = perf_counter()
+            sample = self.replay_buffer.sample(batch_size=self.hparams.batch_size, all_games=all_games)[:self.hparams.batch_size]
+            random.shuffle(sample)
+            sample = train_element_collate_fn(sample)
+            sample = sample.to(self.hparams.device)
+
+            # do not need to call optimizers zero_grad() because we are settig grads to zero in every model
+            self.inference.zero_grad()
+
+            total_loss = self.training_step(sample)
+
+            total_loss.backward()
+            nn.utils.clip_grad_norm_(self.inference.representation.parameters(), 1)
+            nn.utils.clip_grad_norm_(self.inference.prediction.parameters(), 1)
+            nn.utils.clip_grad_norm_(self.inference.dynamic.parameters(), 1)
+
+            for opt in self.optimizers:
+                opt.step()
+
+            train_step_time = perf_counter() - start_time
+
+            self.summary_writer.add_scalar('train/one_step_time', train_step_time, self.global_step)
+            self.summary_writer.add_scalar('train/batch_size', len(sample), self.global_step)
+
+            self.summary_writer.add_scalar('train/total_loss', total_loss, self.global_step)
+            self.summary_writer.add_scalars('train/num_games', {
+                'current': self.replay_buffer.num_games(),
+                'current_max': self.replay_buffer.max_num_games,
+                'config_max': self.hparams.max_training_games,
+            }, self.global_step)
+            self.summary_writer.add_scalar('train/games_received', self.replay_buffer.num_games_received, self.global_step)
+
+            self.global_step += 1
+            self.save_muzero_server_weights()
+
+        self.run_evaluation(save_if_best=True)
+
+    def run_training_offline(self):
         while self.replay_buffer.num_games() == 0:
             time.sleep(1)
 
         self.inference.train(True)
 
         for _ in range(self.hparams.num_training_steps):
+            start_time = perf_counter()
             sample = self.replay_buffer.sample(batch_size=self.hparams.batch_size)[:self.hparams.batch_size]
             random.shuffle(sample)
             sample = train_element_collate_fn(sample)
@@ -200,11 +323,36 @@ class Trainer:
             total_loss = self.training_step(sample)
 
             total_loss.backward()
+            nn.utils.clip_grad_norm_(self.inference.representation.parameters(), 1)
+            nn.utils.clip_grad_norm_(self.inference.prediction.parameters(), 1)
+            nn.utils.clip_grad_norm_(self.inference.dynamic.parameters(), 1)
 
             for opt in self.optimizers:
                 opt.step()
 
+            train_step_time = perf_counter() - start_time
+
+            self.summary_writer.add_scalar('train/one_step_time', train_step_time, self.global_step)
+            self.summary_writer.add_scalar('train/batch_size', len(sample), self.global_step)
+
+            self.summary_writer.add_scalars('train/start_index', {
+                'mean': sample.start_index.float().mean(),
+                'min': sample.start_index.min(),
+                'max': sample.start_index.max(),
+            }, self.global_step)
+            self.summary_writer.add_scalars('train/sample_len', {
+                'mean': sample.sample_len.float().mean(),
+                'min': sample.sample_len.min(),
+                'max': sample.sample_len.max(),
+            }, self.global_step)
+
             self.summary_writer.add_scalar('train/total_loss', total_loss, self.global_step)
+            self.summary_writer.add_scalars('train/num_games', {
+                'current': self.replay_buffer.num_games(),
+                'current_max': self.replay_buffer.max_num_games,
+                'config_max': self.hparams.max_training_games,
+            }, self.global_step)
+            self.summary_writer.add_scalar('train/games_received', self.replay_buffer.num_games_received, self.global_step)
 
             self.global_step += 1
             self.save_muzero_server_weights()
@@ -220,10 +368,17 @@ class Trainer:
         hparams.batch_size = len(self.eval_ds.game_states)
 
         train = simulation.Train(self.game_ctl, self.inference, self.logger, self.summary_writer, 'eval', action_selection_fn)
+        game_state_stack = networks.GameState(hparams.batch_size, hparams, train.game_ctl.network_hparams)
+
         with torch.no_grad():
             active_game_states = self.eval_ds.game_states
             active_player_ids = self.eval_ds.game_player_ids
-            pred_actions, children_visits, root_values = train.run_simulations(active_player_ids, active_game_states)
+            invalid_actions_mask = train.game_ctl.invalid_actions_mask(train.game_ctl.game_hparams, active_game_states)
+
+            game_state_stack.push_game(active_player_ids, active_game_states)
+            game_states = game_state_stack.create_state()
+
+            pred_actions, children_visits, root_values = train.run_simulations(active_player_ids, game_states, invalid_actions_mask)
 
         best_score, good_score, total_best_score, total_good_score = self.eval_ds.evaluate(pred_actions)
 
@@ -314,6 +469,7 @@ def main():
     parser.add_argument('--num_training_steps', type=int, default=40, help='Number of training steps before evaluation')
     parser.add_argument('--checkpoints_dir', type=str, required=True, help='Checkpoints directory')
     parser.add_argument('--game', type=str, required=True, help='Name of the game')
+    parser.add_argument('--online', action='store_true', help='Run online training, i.e. waiting for number of episodes made with the latest model and then training with them')
     FLAGS = parser.parse_args()
 
     #os.environ['CUDA_LAUNCH_BLOCKING'] = '1'
@@ -336,9 +492,14 @@ def main():
         eval_ds = None
 
     trainer = Trainer(module, logger, eval_ds)
+    if FLAGS.online:
+        trainer.replay_buffer.max_num_games = trainer.hparams.max_training_games
 
     while True:
-        trainer.run_training()
+        if FLAGS.online:
+            trainer.run_training_online()
+        else:
+            trainer.run_training_offline()
 
 if __name__ == '__main__':
     main()
